0f5d812796202d5cac8e3f384c02ff798da1510e
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-03-01 15:24:53 -0300

    Create Aula.py

---
+++ Aula.py
@@ -0,0 +1,5 @@
+import os  # Importa o pacote os
+import platform  # Importa o pacote platform
+
+nome = platform.system()  # Pega o nome da plataforma que estamos usando, windows nesse caso.
+print(nome)
